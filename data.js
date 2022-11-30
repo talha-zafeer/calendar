55a96d1d67e8b48e79b52c5fd4eaf7e9ce3cc4d1
Zafeer Saadi <talha.zafeer@gigalabs.co>
2022-11-30 18:37:49 +0500

    Added functionality for half-hours

--- data.js
+++ data.js
@@ -1,3 +1,132 @@
+// "use strict";
+// const events = [
+//   {
+//     startTime: 1.5,
+//     endTime: 3,
+//     name: "Event 1",
+//     location: "Location 1",
+//     hoursIncluded: function () {
+//       const hoursSet = [];
+//       for (let i = this.startTime; i <= this.endTime; i++) {
+//         hoursSet.push(i);
+//       }
+//       return hoursSet;
+//     },
+//   },
+
+//   {
+//     startTime: 0.5,
+//     endTime: 2.5,
+//     name: "Event 2",
+//     location: "Location 2",
+//     hoursIncluded: function () {
+//       const hoursSet = [];
+//       for (let i = this.startTime; i <= this.endTime; i++) {
+//         hoursSet.push(i);
+//       }
+//       return hoursSet;
+//     },
+//   },
+//   {
+//     startTime: 3,
+//     endTime: 4,
+//     name: "Event 3",
+//     location: "Location 3",
+//     hoursIncluded: function () {
+//       const hoursSet = [];
+//       for (let i = this.startTime; i <= this.endTime; i++) {
+//         hoursSet.push(i);
+//       }
+//       return hoursSet;
+//     },
+//   },
+//   {
+//     startTime: 12.5,
+//     endTime: 13.5,
+//     name: "Event 4",
+//     location: "Location 4",
+//     hoursIncluded: function () {
+//       const hoursSet = [];
+//       for (let i = this.startTime; i <= this.endTime; i++) {
+//         hoursSet.push(i);
+//       }
+//       return hoursSet;
+//     },
+//   },
+//   {
+//     startTime: 10,
+//     endTime: 11,
+//     name: "Event 4",
+//     location: "Location 4",
+//     hoursIncluded: function () {
+//       const hoursSet = [];
+//       for (let i = this.startTime; i <= this.endTime; i++) {
+//         hoursSet.push(i);
+//       }
+//       return hoursSet;
+//     },
+//   },
+//   {
+//     startTime: 11.5,
+//     endTime: 12,
+//     name: "Event 4",
+//     location: "Location 4",
+//     hoursIncluded: function () {
+//       const hoursSet = [];
+//       for (let i = this.startTime; i <= this.endTime; i++) {
+//         hoursSet.push(i);
+//       }
+//       return hoursSet;
+//     },
+//   },
+//   {
+//     startTime: 11.5,
+//     endTime: 12,
+//     name: "Event 4",
+//     location: "Location 4",
+//     hoursIncluded: function () {
+//       const hoursSet = [];
+//       for (let i = this.startTime; i <= this.endTime; i++) {
+//         hoursSet.push(i);
+//       }
+//       return hoursSet;
+//     },
+//   },
+//   {
+//     startTime: 11,
+//     endTime: 11.5,
+//     name: "Event 4",
+//     location: "Location 4",
+//     hoursIncluded: function () {
+//       const hoursSet = [];
+//       for (let i = this.startTime; i <= this.endTime; i++) {
+//         hoursSet.push(i);
+//       }
+//       return hoursSet;
+//     },
+//   },
+//   {
+//     startTime: 13,
+//     endTime: 18,
+//     name: "Event 4",
+//     location: "Location 4",
+//     hoursIncluded: function () {
+//       const hoursSet = [];
+//       for (let i = this.startTime; i <= this.endTime; i++) {
+//         hoursSet.push(i);
+//       }
+//       return hoursSet;
+//     },
+//   },
+// ];
+
+// events.sort(
+//   (a, b) => (a.hoursIncluded().length < b.hoursIncluded().length ? 1 : -1)
+//   // a.startTime > b.startTime ? 1 : -1
+// );
+
+// export { events };
+
 "use strict";
 const events = [
   {
@@ -68,7 +197,7 @@ const events = [
 
   {
     startTime: 5,
-    endTime: 8,
+    endTime: 7,
     name: "Event 4",
     location: "Location 4",
     hoursIncluded: function () {
@@ -106,8 +235,8 @@ const events = [
     },
   },
   {
-    startTime: 9,
-    endTime: 10,
+    startTime: 11,
+    endTime: 13,
     name: "Event 4",
     location: "Location 4",
     hoursIncluded: function () {
@@ -121,6 +250,19 @@ const events = [
   {
     startTime: 8,
     endTime: 8.5,
+    name: "Event 4",
+    location: "Location 4",
+    hoursIncluded: function () {
+      const hoursSet = [];
+      for (let i = this.startTime; i <= this.endTime; i++) {
+        hoursSet.push(i);
+      }
+      return hoursSet;
+    },
+  },
+  {
+    startTime: 8.5,
+    endTime: 9,
     name: "Event 4",
     location: "Location 4",
     hoursIncluded: function () {
--- script.js
+++ script.js
@@ -3,12 +3,18 @@ import { events } from "./data.js";
 
 let renderedEvents = [];
 let hourSelector = [];
+let halfHourSelector = [];
 
 function init() {
   for (let i = 0; i < 24; i++) {
-    document.querySelectorAll(`.half-top`)[i].setAttribute("id", `am-${i}`);
-    hourSelector.push(document.querySelector(`#am-${i}`));
+    document.querySelectorAll(`.half-top`)[i].setAttribute("id", `full-${i}`);
+    hourSelector.push(document.querySelector(`#full-${i}`));
+    document
+      .querySelectorAll(`.half-bottom-2`)
+      [i].setAttribute("id", `half-${i}`);
+    halfHourSelector.push(document.querySelector(`#half-${i}`));
   }
+
   for (let i = 0; i < events.length; i++) {
     setEvent(
       events[i].startTime,
@@ -20,66 +26,84 @@ function init() {
 }
 
 const setEvent = (startTime, endTime, name, location) => {
-  const newEvent = hourSelector[Math.floor(startTime)].appendChild(
-    createContent(startTime, name, location)
-  );
+  let newEvent;
   if (startTime < Math.ceil(startTime)) {
-    newEvent.classList.add("event-half");
-    console.log(startTime);
+    newEvent = halfHourSelector[Math.floor(startTime)].appendChild(
+      createContent(startTime, endTime, name, location)
+    );
   } else {
-    newEvent.classList.add("event");
+    newEvent = hourSelector[Math.floor(startTime)].appendChild(
+      createContent(startTime, endTime, name, location)
+    );
   }
-  // if (startTime < Math.ceil(startTime) || endTime < Math.ceil(endTime)) {
-  //   if (startTime == Math.floor(endTime)) {
-  //     newEvent.classList.add("single-half-event-top");
-  //   } else if (Math.ceil(startTime) == endTime) {
-  //     newEvent.classList.add("single-half-event-bot");
-  //   } else {
-  //     newEvent.classList.add("event-half");
-  //   }
-  // } else {
-  //   newEvent.classList.add("event");
-  // }
-  newEvent.style.height = (endTime - startTime) * 100 - 1 + "px";
+
+  newEvent.classList.add("event");
+  const height = (endTime - startTime) * 100 - 3;
+  if (height <= 50) {
+    startTime < Math.ceil(startTime)
+      ? newEvent.classList.add("event-half-bot")
+      : newEvent.classList.add("event-half-top");
+    newEvent.classList.remove("event");
+  }
+  newEvent.style.height = height + "px";
   renderedEvents.push(newEvent);
   align();
+  newEvent.style.height = height + 1 + "px";
 };
 
 const align = () => {
-  let time1, time2;
+  let time, time1, time2;
   for (let i = 0; i < renderedEvents.length; i++) {
     for (let j = i + 1; j < renderedEvents.length; j++) {
-      if (checkOverlapping(renderedEvents[i], renderedEvents[j])) {
-        time1 = parseFloat(
-          renderedEvents[i].parentElement.children[0].innerText.substring(
-            0,
-            renderedEvents[i].children[0].innerText.indexOf(":")
-          )
-        );
-        time2 = parseFloat(
-          renderedEvents[j].children[0].innerText.substring(
-            0,
-            renderedEvents[j].children[0].innerText.indexOf(":")
-          )
-        );
+      if (elementsOverlap(renderedEvents[i], renderedEvents[j])) {
+        time1 = renderedEvents[i].parentElement.children[0].innerText;
+        time2 = renderedEvents[j].children[0].innerText;
+        time = getFloatTime(time1, time2);
+
         document
           .getElementById(renderedEvents[i].parentElement.id)
           .appendChild(renderedEvents[j]);
-
-        renderedEvents[j].style.marginTop = (time2 - time1) * 100 - 10 + "px";
+        renderedEvents[j].style.marginTop =
+          (time[1] - time[0]) * 100 - 9 + "px";
       }
     }
   }
 };
 
-const createContent = (startTime, eventName, eventLocation) => {
+const getFloatTime = (time1, time2) => {
+  const time = [];
+  time1 = time1.substring(0, time1.indexOf(" "));
+  time2 = time2.substring(0, time2.indexOf(" "));
+  time1.includes(":30")
+    ? time.push(parseFloat(time1.replace(":30", ".5")))
+    : time.push(parseFloat(time1));
+  time2.includes(":30")
+    ? time.push(parseFloat(time2.replace(":30", ".5")))
+    : time.push(parseFloat(time2));
+  return time;
+};
+
+const createContent = (startTime, endTime, eventName, eventLocation) => {
   const content = document.createElement("div");
   const time = document.createElement("p");
   const name = document.createElement("h5");
   const location = document.createElement("span");
-  startTime < 12
-    ? (time.innerText = `${startTime}:00 AM`)
-    : (time.innerText = `${startTime}:00 PM`);
+
+  const convertStartTime = startTime.toString();
+  const convertEndTime = endTime.toString();
+
+  if (convertStartTime.includes(".")) {
+    startTime < 12
+      ? (time.innerText = `${convertStartTime.replace(".5", ":30")} AM`)
+      : (time.innerText = `${convertStartTime.replace(".5", ":30")} PM`);
+  } else if (convertStartTime.includes(".")) {
+    startTime < 12
+      ? (time.innerText = `${convertEndTime.replace(".5", ":30")} AM`)
+      : (time.innerText = `${convertEndTime.replace(".5", ":30")} PM`);
+  } else
+    startTime < 12
+      ? (time.innerText = `${startTime}:00 AM`)
+      : (time.innerText = `${startTime}:00 PM`);
 
   name.innerText = eventName;
   location.innerText = eventLocation;
@@ -87,9 +111,9 @@ const createContent = (startTime, eventName, eventLocation) => {
   return content;
 };
 
-const checkOverlapping = (e1, e2) => {
-  const event1 = e1.getBoundingClientRect();
-  const event2 = e2.getBoundingClientRect();
+const elementsOverlap = (el1, el2) => {
+  const event1 = el1.getBoundingClientRect();
+  const event2 = el2.getBoundingClientRect();
 
   return !(
     event1.top > event2.bottom ||
@@ -98,7 +122,5 @@ const checkOverlapping = (e1, e2) => {
     event1.left > event2.right
   );
 };
-
 init();
-
 console.log(events);
